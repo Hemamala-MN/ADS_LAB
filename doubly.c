cd701ecda3fa8acfca3804faba8e9ff6b4b04619
Hemamala-MN <84277583+Hemamala-MN@users.noreply.github.com>
2022-11-14 10:35:24 +0530

    Update doubly.c

--- doubly.c
+++ doubly.c
@@ -1,11 +1,4 @@
 
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
